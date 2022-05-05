97404315b06acf795c544ff8cacd72b6db358261
AzEgyikMark <dornai.mark@students.jedlik.eu>
2022-05-05 11:01:28 +0200

    123456

--- igen.js
+++ igen.js
@@ -3,11 +3,10 @@ var szavaz = []
 
 szavaz[1]="Fidesz:; listavezető: Orbán Viktor 135 / 199 (68%)"
 szavaz[2]="DK–Jobbik–Momentum–MSZP–LMP–Párbeszéd:;  listavezető: Márki-Zay Péter; 57 / 199 (29%)"
-szavaz[3]="Mi Hazánk:; listavezető: Toroczkai László ;6 / 199 (3%)"
+szavaz[3]="Mi Hazánk:; listavezető: Toroczkai László ;6 / 199 (3%)",
 
 
-function info(){
-document.getElementById('eredmenygomb').innerHTML = szavaz[1].split(";");
-document.getElementById('eredmenygomb').innerHTML = szavaz[2].split(";");
-document.getElementById('eredmenygomb').innerHTML = szavaz[3].split(";");
+function ered(){
+    var hely = document.getElementById('szavaz').value
+document.getElementById('eredmenygomb').innerHTML = hely[szavaz].split(";");
 }
