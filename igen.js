716a54e63cd9fb2ef49d560d4fc7cf75b6f66be8
David1829 <91196052+David1829@users.noreply.github.com>
2022-05-05 11:34:45 +0200

    fde

--- igen.js
+++ igen.js
@@ -1,12 +1,2 @@
-
-var szavaz = []
-
-szavaz[1]="Fidesz:; listavezető: Orbán Viktor 135 / 199 (68%)"
-szavaz[2]="DK–Jobbik–Momentum–MSZP–LMP–Párbeszéd:;  listavezető: Márki-Zay Péter; 57 / 199 (29%)"
-szavaz[3]="Mi Hazánk:; listavezető: Toroczkai László ;6 / 199 (3%)",
-
-
-function ered(){
-    var hely = document.getElementById('szavaz').value
-document.getElementById('eredmenygomb').innerHTML = hely[szavaz].split(";");
-}
+function changeText(id) {
+       id.innerHTML = "Fidesz:; listavezető: Orbán Viktor 135 / 199 (68%) DK–Jobbik–Momentum–MSZP–LMP–Párbeszéd:; listavezető: Márki-Zay Péter; 57 / 199 (29%) Mi Hazánk:; listavezető: Toroczkai László ;6 / 199 (3%)".split(";")
